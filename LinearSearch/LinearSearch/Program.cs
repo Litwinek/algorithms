389d189fe7c8d9785b7ca11a05a9e039412fb428
Litwinek <102995549+Litwinek@users.noreply.github.com>
2024-10-19 14:11:11 +0200

    Linear Search
    Linear Search algorithm finding maximum and minimum value of an array.

---
+++ LinearSearch/LinearSearch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LinearSearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in length of array:");
+            int tabLength = int.Parse(Console.ReadLine());
+            int[] array = new int[tabLength];
+
+            for (int i = 0; i < tabLength; i++)
+            {
+                Console.Write("Enter value into table: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            int highest_value = array[0];
+            int lowest_value = array[0];
+            Console.WriteLine("Entered values:");
+            foreach (int item in array)
+            {
+                Console.Write(item + " ");
+                if (item > highest_value)
+                {
+                    highest_value = item;
+                }
+                else if (item < lowest_value) 
+                {
+                    lowest_value = item;
+                }
+            }
+            Console.WriteLine($"\nMin Value: {lowest_value}");
+            Console.WriteLine($"\nMax Value: {highest_value}");
+        }
+    }
+}
